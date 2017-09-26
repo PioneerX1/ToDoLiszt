9ca4961ec8e62742208b69a95566c84931763fc4
Mick Sexton and Saul Artiga <mick+saul@>
2017-09-26 10:58:36 -0700

    add grid item layout for category list

---
+++ app/src/main/java/com/todo/liszt/todoliszt/adapters/CategoryListAdapter.java
@@ -0,0 +1,57 @@
+package com.todo.liszt.todoliszt.adapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.todo.liszt.todoliszt.R;
+
+public class CategoryListAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private String[] mCategoryNames;
+
+    public CategoryListAdapter(Context context, String[] categoryNames) {
+        this.mContext = context;
+        this.mCategoryNames = categoryNames;
+    }
+
+
+    @Override
+    public int getCount() {
+        return mCategoryNames.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+            gridView = inflater.inflate(R.layout.category_list_grid_item, null);
+            TextView categoryName = (TextView) gridView
+                    .findViewById(R.id.category_grid_item);
+            categoryName.setText("I have a family");
+        } else {
+            gridView = (View) convertView;
+        }
+
+        return gridView;
+    }
+}
--- app/src/main/java/com/todo/liszt/todoliszt/ui/MainActivity.java
+++ app/src/main/java/com/todo/liszt/todoliszt/ui/MainActivity.java
@@ -5,21 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 
 import com.todo.liszt.todoliszt.R;
+import com.todo.liszt.todoliszt.adapters.CategoryListAdapter;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    @Bind(R.id.categoryGridView) GridView mCategoryGridView;
     @Bind(R.id.newCategoryButton) Button mNewCategoryButton;
+
+    String[] categoryNames = new String[] {"Wok", "Kvlt", "Handbeezies", "NigNogs"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mCategoryGridView.setAdapter(new CategoryListAdapter(this, categoryNames));
 
         mNewCategoryButton.setOnClickListener(this);
     }
